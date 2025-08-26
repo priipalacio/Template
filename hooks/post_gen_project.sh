a9d990bc6a641a6b120553367d93493d39a6bab8
priipalacio <54598457+priipalacio@users.noreply.github.com>
2025-08-26 17:44:23 -0300

    update script

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,47 +5,47 @@ echo "$(pwd)"
 cd Demo
 echo "$(pwd)"
 cat > "project.yml"<<EOF
-name: {{cookiecutter.project_name}}Demo
+name: {{cookiecutter.module_name}}Demo
 options:
   minimumXcodeGenVersion: 2.0.0
   deploymentTarget:
     iOS: "15.0"
 
 packages:
-  {{cookiecutter.project_name}}:
-    path: ../{{cookiecutter.project_name}}
+  {{cookiecutter.module_name}}:
+    path: ../{{cookiecutter.module_name}}
 
 targets:
-  {{cookiecutter.project_name}}Demo:
+  {{cookiecutter.module_name}}Demo:
     type: application
     platform: iOS
     bundleId: {{cookiecutter.bundle_identifier}}
     info:
-      path: {{cookiecutter.project_name}}Demo/Info.plist
+      path: {{cookiecutter.module_name}}Demo/Info.plist
     settings:
       base:
         PRODUCT_BUNDLE_IDENTIFIER: {{cookiecutter.bundle_identifier}}
-        INFOPLIST_FILE: {{cookiecutter.project_name}}Demo/Info.plist
+        INFOPLIST_FILE: {{cookiecutter.module_name}}Demo/Info.plist
     sources:
-      - path: ./{{cookiecutter.project_name}}Demo
+      - path: ./{{cookiecutter.module_name}}Demo
     dependencies:
-      - package: {{cookiecutter.project_name}}
+      - package: {{cookiecutter.module_name}}
 
-  {{cookiecutter.project_name}}DemoTests:
+  {{cookiecutter.module_name}}DemoTests:
     type: bundle.unit-test
     platform: iOS
     sources:
-      - path: ./{{cookiecutter.project_name}}DemoTests
+      - path: ./{{cookiecutter.module_name}}DemoTests
     dependencies:
-      - target: {{cookiecutter.project_name}}Demo
+      - target: {{cookiecutter.module_name}}Demo
 
-  {{cookiecutter.project_name}}DemoUITests:
+  {{cookiecutter.module_name}}DemoUITests:
     type: bundle.ui-testing
     platform: iOS
     sources:
-      - path: ./{{cookiecutter.project_name}}DemoUITests
+      - path: ./{{cookiecutter.module_name}}DemoUITests
     dependencies:
-      - target: {{cookiecutter.project_name}}Demo
+      - target: {{cookiecutter.module_name}}Demo
 EOF
 
 if command -v xcodegen >/dev/null 2>&1; then
